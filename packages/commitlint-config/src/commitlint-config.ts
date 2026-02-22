7a8680aba9429438098d4a09b42d9fff000a0b20
Eric King <eric@webdeveric.com>
2026-02-22 16:49:38 -0600

    chore(commitlint-config): removed comments

--- packages/commitlint-config/src/commitlint-config.ts
+++ packages/commitlint-config/src/commitlint-config.ts
@@ -9,16 +9,11 @@ const config = {
     'cspell/subject': [2, 'always'],
     'cspell/body': [2, 'always'],
     'cspell/footer': [2, 'always'],
-    // This rule ensures that a ticket number is included in the commit message.
-    // 'subject-contains-ticket': [2, 'always'],
     'header-max-length': [2, 'always', 100],
     'body-max-line-length': [2, 'always', 250],
-    // This ensures that the scope is always present and follows the correct format.
     'scope-case': [2, 'always', ['lower-case', 'upper-case']],
-    // This ensures that the subject is not empty and starts with a lowercase letter.
     'subject-empty': [2, 'never'],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    // This rule ensures that the type is one of the conventional commit types and is in lower-case.
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'type-enum': [
